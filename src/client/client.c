bf36354490f2fbdd5c6751c31ecae96cd6743a0d
Hitin Sarin <hitinsarin3091@gmail.com>
2020-06-16 16:16:33 -0400

    Client commit

--- src/client/client.c
+++ src/client/client.c
@@ -1,9 +1,11 @@
+/* This includes the first function get_sockaddr which converts the Server's IP address and Port into a usable form which further creates Socket. After creating the Sockets, the next function open_connection will establish a connection to the server using sockets. */
+
 #include "common.h"
 
 /*
- * The function get_sockaddr converts the server's address and port into a form usable to create a 
- * socket
+ * The function get_sockaddr converts the server's address and port into a form usable to create a socket.
 */
+
 struct addrinfo* get_sockaddr(struct addrinfo hints, const char* hostname, const char *port){
   
   struct addrinfo* results;
@@ -20,24 +22,29 @@ struct addrinfo* get_sockaddr(struct addrinfo hints, const char* hostname, const
 /*
  * The function open_connection create socket and establishes a connection to the server
 */
+
 int open_connection(struct addrinfo* addr_list){
 
   struct addrinfo* addressInfo;
   int sockfd, connectfd;
 
-  //Iterate through each addr info in the list; Stop when we successully connect to one
+  /* Iterate through each addr info in the list; Stop when we successully connect to one */
   for (addressInfo = addr_list; addressInfo != NULL; addressInfo = addressInfo->ai_next)
     {
       sockfd = socket(addressInfo->ai_family, addressInfo->ai_socktype, addressInfo->ai_protocol);
-      // Try the next address since the socket operation failed
-      if (sockfd == -1) 
+	  
+      /* Try the next address since the socket operation failed */
+	  
+      if (sockfd == -1){
         continue;
-
+		}
       connectfd = connect(sockfd,addressInfo->ai_addr, addressInfo->ai_addrlen);
-       //Stop iterating of we are able to connect to the server
-      if ( connectfd != -1) 
+	  
+       /* Stop iterating of we are able to connect to the server */
+      if ( connectfd != -1){
         break;
-    }
+		}
+	}
 
   freeaddrinfo(addr_list);
 
@@ -49,41 +56,4 @@ int open_connection(struct addrinfo* addr_list){
   }
 
   return sockfd;
-}
-
-int main(int argc, char *argv[]){
-
-  char sendBuff[MAXLINE];
-  char recvBuff[MAXLINE];
-  struct sockaddr_in serv_addr;
-  int read_in, sockfd;
-  struct addrinfo hints;
-
-  if(argc != 3){
-    err_msg_die("\n Usage: %s hostname port number \n",argv[0]);
-  }
-
-  memset(&hints, 0, sizeof(struct addrinfo));
-
-  hints.ai_family = AF_INET;          //Return socket address for the server's IPv4 addresses
-  hints.ai_socktype = SOCK_STREAM;    //Return TCP socket addresses
-  struct addrinfo* results = get_sockaddr(argv[1], argv[2]);
-  sockfd= open_connection(results);
-
-  printf("Give me Something to send: ");
-  fgets(sendBuff, sizeof(sendBuff), stdin);
-  
-  if (send (sockfd, sendBuff, strlen(sendBuff), 0) < 0){
-    err_msg_die("send error");
-  }
-
-  //Receive a reply from the server
-  if ((read_in = recv(sockfd , recvBuff , MAXLINE-1 , 0)) < 0){
-    err_msg_die("read error");
-  }
-  
-  printf("%s",recvBuff);
-  // Closing the connection
-  close (sockfd);
-  return 0;
 }
