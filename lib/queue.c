9dba05fc57f8f3ab6f09f6e9ea7871ebf9c68f34
sakky27 <saksham.mal1992@gmail.com>
2020-06-21 19:02:37 -0400

    Adding Queue Implementaion

---
+++ lib/queue.c
@@ -0,0 +1,80 @@
+/* C program for array implementation of queue */
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <limits.h> 
+#include "../include/queue.h"
+
+/* A structure to represent a queue */
+struct Queue 
+{ 
+    int front, rear, size; 
+    unsigned capacity; 
+    int* array; 
+}; 
+  
+// function to create a queue of given capacity.  
+// It initializes size of queue as 0 
+struct Queue* createQueue(unsigned capacity) 
+{ 
+    struct Queue* queue = (struct Queue*) malloc(sizeof(struct Queue)); 
+    queue->capacity = capacity;
+    queue->rear = capacity - 1;  
+    queue->front = queue->size = 0;  
+    queue->array = (int*) malloc(queue->capacity * sizeof(int)); 
+    return queue; 
+} 
+  
+// Queue is full when size becomes equal to the capacity  
+int isFull(struct Queue* queue) {
+    if (queue->size == queue->capacity){
+    return 1;
+    }
+    return 0; 
+} 
+  
+// Queue is empty when size is 0 
+int isEmpty(struct Queue* queue){  
+    if (queue->size == 0){
+    return 1;
+    }
+    return 0; 
+} 
+  
+// Function to add an item to the queue.   
+// It changes rear and size 
+void enqueue(struct Queue* queue, int item) 
+{ 
+    if (isFull(queue)) 
+        return; 
+    queue->rear = (queue->rear + 1)%queue->capacity; 
+    queue->array[queue->rear] = item; 
+    queue->size = queue->size + 1; 
+} 
+  
+// Function to remove an item from queue.  
+// It changes front and size 
+int dequeue(struct Queue* queue) 
+{ 
+    if (isEmpty(queue)) 
+        return INT_MIN; 
+    int item = queue->array[queue->front]; 
+    queue->front = (queue->front + 1)%queue->capacity; 
+    queue->size = queue->size - 1; 
+    return item; 
+} 
+  
+// Function to get front of queue 
+int front(struct Queue* queue) 
+{ 
+    if (isEmpty(queue)) 
+        return INT_MIN; 
+    return queue->array[queue->front]; 
+} 
+  
+// Function to get rear of queue 
+int rear(struct Queue* queue) 
+{ 
+    if (isEmpty(queue)) 
+        return INT_MIN; 
+    return queue->array[queue->rear]; 
+} 
